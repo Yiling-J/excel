d2b111bbec446090498644c72081e1b5a1e6dfa0
Yiling-J <njjyl723@gmail.com>
2024-01-25 15:36:03 +0800

    bypass standard numfmtid

--- lib/src/parser/parse.dart
+++ lib/src/parser/parse.dart
@@ -297,8 +297,7 @@ class Parser {
           final numFmtId = int.parse(node.getAttribute('numFmtId')!);
           final formatCode = node.getAttribute('formatCode')!;
           if (numFmtId < 164) {
-            throw Exception(
-                'custom numFmtId starts at 164 but found a value of $numFmtId');
+            return;
           }
 
           _excel._numFormats
