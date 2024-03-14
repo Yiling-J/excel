89d442c35741e6aa3b46830878efbb92b3333145
Yiling-J <njjyl723@gmail.com>
2024-03-14 17:41:57 +0800

    add text format support

--- lib/src/number_format/num_format.dart
+++ lib/src/number_format/num_format.dart
@@ -333,7 +333,7 @@ class CustomNumericNumFormat extends NumericNumFormat
         null => true,
         FormulaCellValue() => true,
         IntCellValue() => true,
-        TextCellValue() => false,
+        TextCellValue() => true,
         BoolCellValue() => true,
         DoubleCellValue() => true,
         DateCellValue() => false,
